bda7dd63ee76ffd8e7ebe5d6dab816f90acf19a2
Akshat Shukla <com.akshat.dev@gmail.com>
2025-02-27 03:08:01 +0530

    [53] [Time Beats: 100.00%] [Memory Beats: 52.95%] - LeetPush

---
+++ 53-Maximum-Subarray.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int sum=0, maxsum=INT_MIN, minsum=0, ans;
+
+        for(int i=0; i<nums.size();i++){
+
+            sum+=nums[i];
+
+            maxsum=max(sum,maxsum);
+
+            if(sum<0){
+                sum=0;
+            }
+        }
+        ans =  maxsum;
+        return ans;
+    }
+    
+};
